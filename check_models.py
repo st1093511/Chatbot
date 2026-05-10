ff36e531c407c59019a560a0a6729f6df4287aad
st1093511 <st1093511@ceid.webmail.com>
2026-05-10 15:35:47 +0300

    small changes

---
+++ check_models.py
@@ -0,0 +1,6 @@
+﻿import os
+from google import genai
+client = genai.Client(api_key='AIzaSyBaXt09sOEcI0p39GwCmsn25LDdsMS-JH8')
+for m in client.models.list():
+    if 'embed' in m.name.lower():
+        print(m.name)
